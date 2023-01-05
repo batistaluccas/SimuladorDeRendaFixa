f2bbe2c4c8b402f8e926b86abcb518a9358eaa07
Lucas <lucaslimabatista.eng@gmail.com>
2023-01-05 16:45:28 -0300

    tabela funcionando

--- calculate.js
+++ calculate.js
@@ -3,7 +3,7 @@ const table = document.querySelector('table');
 
 
 btn.onclick = function(){
-    
+
     let startingAmount = document.querySelector('#startingAmount').value || 0;
     let monthlyAmount = document.querySelector('#monthlyAmount').value || 0;
     let interestRates = document.querySelector('#interestRates').value || 0;
@@ -21,18 +21,27 @@ btn.onclick = function(){
         document.querySelectorAll('tr').forEach((element) => element.remove())
     } 
 
-    function calculate (){
-
+    function calculateResult (){
         
         let investAmount = parseFloat(((a*n) + C).toFixed(2));
         invest.value = investAmount;
         let accumulateAmount = parseFloat(((C * ((1 + (i / 100)) ** n)) + (a * (((((1 + (i / 100))) ** n )- 1) / (i / 100)))).toFixed(2));
         accumulate.value = accumulateAmount;
         let fromRatesAmount = parseFloat((accumulateAmount - investAmount).toFixed(2));
-        fromRates.value = fromRatesAmount;
+        fromRates.value = fromRatesAmount;   
     }
-        
-    
+
+    function calculateAmountByMonth(){
+        totalByMonth = new Array(n);
+        for (let counter = 1; counter <= n; counter++){
+            totalByMonth [(counter-1)] = (((C * ((1 + (i / 100)) ** counter)) + (a * (((((1 + (i / 100))) ** counter )- 1) / (i / 100))))
+            -
+            ((C * ((1 + (i / 100)) ** (counter-1))) + (a * (((((1 + (i / 100))) ** (counter-1))- 1) / (i / 100))))) 
+            - 
+            a;    
+        }
+    }            
+
     function createTable(){  
         
         for (let counter = 0; counter < n; counter++){
@@ -41,14 +50,15 @@ btn.onclick = function(){
             const newMonth = document.createElement('td');
             newMonth.innerText = (counter + 1) + "° mês";
             const newData = document.createElement('td');
-            newData.innerText = "R$ 0,00";
+            newData.innerText = "R$ "+parseFloat(totalByMonth[counter]).toFixed(2);
             table.append(newRow);
             newRow.append(newMonth);
             newRow.append(newData);            
         }
     }
     
-    calculate();
+    calculateResult();
+    calculateAmountByMonth();
     createTable();    
 
     document.querySelector('section').hidden = false;
