28a7cc11c12859e91768427f3091e18eb03a3759
Lucas <lucaslimabatista.eng@gmail.com>
2023-01-05 12:37:25 -0300

    tabela só com o mês

--- calculate.js
+++ calculate.js
@@ -1,30 +1,58 @@
 const btn = document.querySelector('#btn');
-const tBody = document.querySelector('tbody');
-const newRow = document.createElement('tr');
-const newMonth = document.createElement('td');
-newMonth.innerText = "x° mês";
-const newData = document.createElement('td');
-newData.innerText = "R$ 0,00";
+const table = document.querySelector('table');
+console.log(table.firstChild)
 
-tBody.append(newRow); /* para aparecer antes deve ser .prepend */
-newRow.append(newMonth);
-newRow.append(newData);
 
 btn.onclick = function(){
 
-    let startingAmount = document.querySelector('#startingAmount').value || 0;
-    let monthlyAmount = document.querySelector('#monthlyAmount').value || 0;
-    let interestRates = document.querySelector('#interestRates').value || 0;
-    let period = document.querySelector('#period').value || 0;
-    let accumulate = document.querySelector('#accumulate')
-    let C = parseFloat(startingAmount);
-    let i = parseFloat(interestRates);
-    let n = parseFloat(period);
-    let a = parseFloat(monthlyAmount);
+    if (table.childElementCount){
+        document.querySelectorAll('tr').forEach((element) => element.remove())
+    } 
 
-    let invest = (a*n) + C;
-    let accumulateAmount = parseFloat(((C * ((1 + (i / 100)) ** n)) + (a * (((((1 + (i / 100))) ** n )- 1) / (i / 100)))).toFixed(2));
-    accumulate.value = accumulateAmount;
+    function calculate (){
+
+        let startingAmount = document.querySelector('#startingAmount').value || 0;
+        let monthlyAmount = document.querySelector('#monthlyAmount').value || 0;
+        let interestRates = document.querySelector('#interestRates').value || 0;
+        let period = document.querySelector('#period').value || 0;
+        let invest = document.querySelector('#invest');
+        let accumulate = document.querySelector('#accumulate');
+        let fromRates = document.querySelector('#fromRates');
+        
+        let C = parseFloat(startingAmount);
+        let i = parseFloat(interestRates);
+        let n = parseFloat(period);
+        let a = parseFloat(monthlyAmount);
+        
+        let investAmount = parseFloat(((a*n) + C).toFixed(2));
+        invest.value = investAmount;
+        let accumulateAmount = parseFloat(((C * ((1 + (i / 100)) ** n)) + (a * (((((1 + (i / 100))) ** n )- 1) / (i / 100)))).toFixed(2));
+        accumulate.value = accumulateAmount;
+        let fromRatesAmount = parseFloat((accumulateAmount - investAmount).toFixed(2));
+        fromRates.value = fromRatesAmount;
+    }
+        
+    
+    function createTable(){  
+        
+        for (let counter = 0; counter < n; counter++){
+            
+            const newRow = document.createElement('tr');
+            const newMonth = document.createElement('td');
+            newMonth.innerText = (counter + 1) + "° mês";
+            const newData = document.createElement('td');
+            newData.innerText = "R$ 0,00";
+            table.append(newRow);
+            newRow.append(newMonth);
+            newRow.append(newData);            
+        }
+    }
+    
+    calculate();
+    createTable();    
+
+    document.querySelector('section').hidden = false;
+    document.querySelector('#table').hidden = false;
 }
 
 
